14803bd2e9b04b05baba6d8c39bab15eb5cd6893
Ben Constable <ben@benconstable.co.uk>
2013-08-13 16:38:33 +0100

    Big refactor and changes

--- src/Ev/Phingy/ComposerScripts.php
+++ src/Ev/Phingy/ComposerScripts.php
@@ -10,6 +10,14 @@ use Composer\Script\Event;
 class ComposerScripts
 {
     /**
+     * @var array $templates Array of available project templates
+     */
+    private static $templates = array(
+        'default',
+        'typo3'
+    );
+
+    /**
      * Create template build files if they don't exist.
      * 
      * @param Composer\Script\Event $event
@@ -20,7 +28,7 @@ class ComposerScripts
         if (!file_exists('build.xml')) {
             $event->getIO()->write('build.xml does not exist, creating template');
 
-            copy('vendor/3ev/phingy/scripts/templates/build.xml', 'build.xml');
+            symlink('vendor/3ev/phingy/scripts/build.xml', 'build.xml');
         } else {
             $event->getIO()->write('build.xml exists, template not needed');
         }
@@ -28,8 +36,17 @@ class ComposerScripts
         // Create project build file if it doesn't exist
         if (!file_exists('config/project.xml')) {
             $event->getIO()->write('config/project.xml does not exist, creating template');
+            try {
+                $temps   = implode(',', self::$templates);
+                $default = self::$templates[0];
 
-            copy('vendor/3ev/phingy/scripts/templates/project.xml', 'config/project.xml');
+                $template = $event->getIO()->ask(
+                    "Which template would you like to use ({$temps}) [{$default}] ? ", $default);
+            } catch (Exception $e) {
+                $template = $default;
+            }
+
+            copy("vendor/3ev/phingy/scripts/templates/{$template}.xml", 'config/project.xml');
         } else {
             $event->getIO()->write('config/project.xml exists, template not needed');
         }
