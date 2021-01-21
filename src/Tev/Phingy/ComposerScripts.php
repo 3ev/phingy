b296dd585a423609867d1d01df9496d1e651a9cb
Dimitrios Chantzis <dimitrios@3ev.com>
2021-01-21 14:25:45 +0000

    Reverting tests

--- src/Tev/Phingy/ComposerScripts.php
+++ src/Tev/Phingy/ComposerScripts.php
@@ -1,7 +1,7 @@
 <?php
 namespace Tev\Phingy;
 
-//use Composer\Script\Event;
+use Composer\Script\Event;
 
 /**
  * Installation scripts.
@@ -20,7 +20,7 @@ class ComposerScripts
     /**
      * Create template build files if they don't exist.
      *
-     * @param $event
+     * @param \Composer\Script\Event $event
      * @return void
      */
     public static function postInstall($event)
